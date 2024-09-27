b8268f82f8272a3ba8c32ed8f16c0bd6ad70aa9f
1ksosa <Sosaqasem@gmail.com>
2024-09-26 17:32:07 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+//Create a string for the user
+const str ="You have received this message because you have been chosen to open an important vault. Here is the secret"
+//declaring variable num1 with +
+let num1 = 2+8
+//declaring variable num1 with *
+let num2 = 10 * 4
+//declaring variable num1 with -
+let num3 = 40 - 1
+//checking three assigned variables
+//console.log(num1, num2, num3)
+//creating the dialog box
+alert (`${str} ${num1}, ${num2}, ${num3}.`)
